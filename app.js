64ac25a694a6609b90de2bcdd9b26cbc65249e02
Patrick Sheron Moucle <mouclepatrick@protonmail.com>
2022-08-30 11:50:35 +0200

    feat:migrate to fly.io (#13)
    Due to the recent change of the heroku platform, no more free  tiers.

--- app.js
+++ app.js
@@ -1,73 +1,76 @@
 'use strict';
 
-import { join } from 'desm';
+import {join} from 'desm';
 import AutoLoad from '@fastify/autoload';
 import Cookie from '@fastify/cookie';
 import Cors from '@fastify/cors';
 import Env from '@fastify/env';
 import lightrun from 'lightrun';
-import { ALLOWED_ORIGIN } from './constants.js';
+
 lightrun.start({
-  lightrunSecret: 'c011f943-383c-4509-81e3-38afdaffcab0',
+    lightrunSecret: 'c011f943-383c-4509-81e3-38afdaffcab0',
 });
 export default async function (fastify, opts) {
-  const envOption = {
-    dotenv: {
-      path: `${import.meta.url}/.env`,
-      debug: false,
-    },
-    schema: {
-      type: 'object',
-      required: [
-        'REDIRECT_URI',
-        'CLIENT_SECRET',
-        'CLIENT_ID',
-        'SCOPE',
-        'STATE_KEY',
-      ],
-      properties: {
-        REDIRECT_URI: {
-          type: 'string',
+    const envOption = {
+        dotenv: {
+            debug: false,
         },
-        CLIENT_ID: {
-          type: 'string',
+        schema: {
+            type: 'object',
+            required: [
+                'REDIRECT_URI',
+                'CLIENT_SECRET',
+                'CLIENT_ID',
+                'SCOPE',
+                'STATE_KEY',
+                'FRONTEND_URI'
+            ],
+            properties: {
+                REDIRECT_URI: {
+                    type: 'string',
+                },
+                CLIENT_ID: {
+                    type: 'string',
+                },
+                CLIENT_SECRET: {
+                    type: 'string',
+                },
+                SCOPE: {
+                    type: 'string',
+                },
+                STATE_KEY: {
+                    type: 'string',
+                },
+                FRONTEND_URI: {
+                    type: 'string'
+                }
+            },
         },
-        CLIENT_SECRET: {
-          type: 'string',
-        },
-        SCOPE: {
-          type: 'string',
-        },
-        STATE_KEY: {
-          type: 'string',
-        },
-      },
-    },
-  };
+    };
 
-  fastify.register(Env, envOption);
-  fastify.register(Cookie);
+    fastify.register(Env, envOption);
+    fastify.register(Cookie);
 
-  fastify.register(Cors, {
-    credentials: true,
-    origin: ALLOWED_ORIGIN,
-    methods: ['GET'],
-  });
+    fastify.register(Cors, {
+        credentials: true,
+        origin: process.env.FRONTEND_URI,
+        methods: ['GET'],
+    });
 
-  // Do not touch the following lines
+    // Do not touch the following lines
 
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
-  fastify.register(AutoLoad, {
-    dir: join(import.meta.url, 'plugins'),
-    options: Object.assign({}, opts),
-  });
+    // This loads all plugins defined in plugins
+    // those should be support plugins that are reused
+    // through your application
+    fastify.register(AutoLoad, {
+        dir: join(import.meta.url, 'plugins'),
+        options: Object.assign({}, opts),
+    });
 
-  // This loads all plugins defined in routes
-  // define your routes in one of these
-  fastify.register(AutoLoad, {
-    dir: join(import.meta.url, 'routes'),
-    options: Object.assign({}, opts),
-  });
+    // This loads all plugins defined in routes
+    // define your routes in one of these
+    fastify.register(AutoLoad, {
+        dir: join(import.meta.url, 'routes'),
+        options: Object.assign({}, opts),
+    });
 }
--- constants.js
+++
@@ -1,6 +0,0 @@
-const ALLOWED_ORIGIN =
-  process.env.NODE_ENV === 'production'
-    ? 'https://spotifile.netlify.app'
-    : 'http://localhost:5000';
-const DYNO_URL = 'https://server-spotify-profile.herokuapp.com/';
-export { ALLOWED_ORIGIN, DYNO_URL };
